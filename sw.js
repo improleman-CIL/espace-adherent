284b065cda9dd8d0a9c7392aa0fc27220bed7a7e
improleman-CIL <improleman@gmail.com>
2026-08-31 12:11:50 +0200

    Ajoute l’icône CIL sur fond blanc

--- sw.js
+++ sw.js
@@ -1,10 +1,10 @@
-const CACHE_NAME = 'cil-adherents-github-v1';
+const CACHE_NAME = 'cil-adherents-github-v2';
 const BASE_PATH = new URL('./', self.location.href).pathname;
 const CORE_ASSETS = [
   BASE_PATH,
   `${BASE_PATH}manifest.webmanifest`,
-  `${BASE_PATH}icon-192.png`,
-  `${BASE_PATH}icon-512.png`
+  `${BASE_PATH}icon-192-white.png`,
+  `${BASE_PATH}icon-512-white.png`
 ];
 
 self.addEventListener('install', event => {
